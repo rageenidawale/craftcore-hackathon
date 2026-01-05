c6a1e5f947b300a744b8dd757dad414ec4baef5b
Rageeni Dawale <rageenidawale@gmail.com>
2026-01-05 17:42:53 +0530

    Update allowed hosts in settings

--- craftcore/craftcore/settings.py
+++ craftcore/craftcore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["craftcore-6oqi.onrender.com", ".onrender.com", "localhost", "127.0.0.1",]
 
 
 # Application definition
