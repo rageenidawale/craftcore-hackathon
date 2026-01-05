5372663412df274d8baf274a78eed63064f1f18f
Rageeni Dawale <rageenidawale@gmail.com>
2026-01-05 18:02:23 +0530

    Add health checks to settings

--- craftcore/craftcore/urls.py
+++ craftcore/craftcore/urls.py
@@ -18,8 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def health_check():
+    return HttpResponse("OK")
 
 urlpatterns = [
+    path("", health_check),
     path('admin/', admin.site.urls),
     path("", include("core.urls")),
     path("", include("accounts.urls")),
